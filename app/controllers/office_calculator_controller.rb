3eca26ec75c56a12cae85cf53b8f44134d6c039f
Mikael <mikael.uusitalo@gmail.com>
2024-10-09 11:28:14 +0200

    Loads index, step 1 and step 2

--- app/controllers/office_calculator_controller.rb
+++ app/controllers/office_calculator_controller.rb
@@ -1,53 +1,34 @@
 class OfficeCalculatorController < ApplicationController
     before_action :load_calculator_config
+    before_action :set_current_step
+    before_action :set_questions
+    before_action :set_active_locations, only: [:index, :start, :next_step]
 
     def index
-        @current_step = session[:current_step] || 'step_1'
-        @questions = @current_step == 'start' ? nil : @calculator_config['calculator_steps'][@current_step]
-        
-        # Add this line to fetch active locations for step 1
-        @active_locations = Location.all if @current_step == 'step_1'
+        session[:current_step] = 'start'
+        @current_step = 'start'
     end
 
     def start
         session[:current_step] = 'step_1'
         @current_step = 'step_1'
-        @questions = @calculator_config['calculator_steps'][@current_step]
-
-        respond_to do |format|
-            format.turbo_stream do
-                render turbo_stream: turbo_stream.replace(
-                    "calculator_content",
-                    partial: @current_step,
-                    locals: { questions: @questions, current_step: @current_step, active_locations: @active_locations }
-                )
-            end
-        end
+        set_questions
+        set_active_locations
+        render_step
     end
 
     def next_step
-        @current_step = params[:current_step].presence || 'step_1'
+        @current_step = params[:current_step]
+        save_form_data_to_session
         next_step_number = @current_step.split('_').last.to_i + 1
         @next_step = "step_#{next_step_number}"
 
-        # Save form data to session
-        save_form_data_to_session
-
         if @calculator_config['calculator_steps'].key?(@next_step)
             @current_step = @next_step
-            @questions = @calculator_config['calculator_steps'][@current_step]
-        
-            respond_to do |format|
-                format.turbo_stream do
-                    render turbo_stream: turbo_stream.replace(
-                        "calculator_content",
-                        partial: @current_step,
-                        locals: { questions: @questions, current_step: @current_step, active_locations: @active_locations }
-                    )
-                end
-            end
+            session[:current_step] = @current_step
+            set_questions
+            render_step
         else
-            # Handle completion of all steps
             redirect_to office_calculator_result_path
         end
     end
@@ -62,9 +43,39 @@ class OfficeCalculatorController < ApplicationController
         @calculator_config = {}
     end
 
+    def set_current_step
+        @current_step = session[:current_step] || 'step_1'
+    end
+
+    def set_questions
+        @questions = @calculator_config.dig('calculator_steps', @current_step)
+        if @questions.nil?
+            Rails.logger.error "No questions found for step: #{@current_step}"
+            @questions = {}  # Set to an empty hash to avoid nil errors
+        end
+        Rails.logger.debug "Questions for step #{@current_step}: #{@questions.inspect}"
+    end
+
+    def set_active_locations
+        @active_locations ||= Location.find_each.to_a if @current_step == 'step_1'
+    end
+
+    def render_step
+        respond_to do |format|
+            format.turbo_stream do
+                render turbo_stream: turbo_stream.replace(
+                    "calculator_content",
+                    partial: @current_step,
+                    locals: { questions: @questions, current_step: @current_step }
+                )
+            end
+            format.html { render partial: @current_step, locals: { questions: @questions, current_step: @current_step } }
+        end
+    end
+
     def save_form_data_to_session
         current_step_config = @calculator_config['calculator_steps'][@current_step]
-        current_step_config.each do |field, data|
+        current_step_config&.each do |field, data|
             if data['options'].is_a?(Hash)
                 data['options'].each do |option_key, _|
                     session["calculator_#{field}_#{option_key}"] = params["#{field}_#{option_key}"]
--- app/javascript/controllers/office_calculator_controller.js
+++ app/javascript/controllers/office_calculator_controller.js
@@ -4,6 +4,8 @@ export default class extends Controller {
   static targets = ["step", "prevButton", "nextButton", "submit"]
 
   connect() {
+    console.log("Office Calculator controller connected");
+    document.addEventListener("turbo:before-stream-render", this.logStreamRender);
     this.currentStep = 0
     this.updateButtonVisibility()
   }
@@ -29,5 +31,13 @@ export default class extends Controller {
     this.prevButtonTarget.classList.toggle('hidden', this.currentStep === 0)
     this.nextButtonTarget.classList.toggle('hidden', this.currentStep === this.stepTargets.length - 1)
     this.submitTarget.classList.toggle('hidden', this.currentStep !== this.stepTargets.length - 1)
+  }
+
+  disconnect() {
+    document.removeEventListener("turbo:before-stream-render", this.logStreamRender);
+  }
+
+  logStreamRender = (event) => {
+    console.log("Turbo Stream received:", event.detail);
   }
 }
